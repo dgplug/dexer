94e8b288c8b2f4206d411ca9288469783efa9bec
Kuntal Majumder <hellozee@disroot.org>
2018-10-26 21:46:39 +0530

    Script updated to show results with proper formatting

--- ui/js/script.js
+++ ui/js/script.js
@@ -1,7 +1,26 @@
-$(document).ready(function(){
-    $(".search-field").keyup(function(){
-        $.ajax({url: "/search/"+$(".search-field").val(), success: function(result){
-            $(".search-results").html(result);
-        }});
-    });
-});
+function keyuphandler(){
+    $(".search-field").keyup(search);
+}
+
+function search(){
+    $.ajax({url: "/search/"+$(".search-field").val(), success: receiver});
+}
+
+function receiver(result){
+    var json = jQuery.parseJSON(result);
+    if(json.length === 0){
+        $(".search-result-header").html("Sorry no results found.");
+        return;
+    }
+    $(".search-result-header").html("Results found: " + json.length);
+    var temp = "";
+    for(var i in json){
+        if(i === 1){
+            temp = '<div class="entry">' + JSON.stringify(json[i].id) + "</div>";
+        }
+        temp += '<div class="entry">' + JSON.stringify(json[i].id) + "</div>";
+    }
+    $(".search-results").html(temp);
+}
+
+$(document).ready(keyuphandler);
