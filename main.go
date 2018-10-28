72924a74bf7f9b44e71c40c9a8d8e4ce32c25d83
Kuntal Majumder <hellozee@disroot.org>
2018-10-28 12:54:43 +0530

    Added a -h flag for printing the help information

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/farhaanbukhsh/file-indexer/conf"
 	"github.com/farhaanbukhsh/file-indexer/indexer"
@@ -17,6 +18,7 @@ import (
 
 var configFlag = flag.String("c", "", "Use alternative config file.")
 var verboseFlag = flag.Bool("v", false, "Verbosely print the log output to Standard Output.")
+var helpFlag = flag.Bool("h", false, "Print this help information.")
 
 var config conf.Configuration
 var lg *logger.Logger
@@ -24,6 +26,12 @@ var templates []string
 
 func init() {
 	flag.Parse()
+
+	if *helpFlag == true {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
 	lg = logger.NewLogger("logfile", *verboseFlag)
 	config = conf.NewConfig(*configFlag, lg)
 
